3ceb73d6ee185fc841e7ce1d6415f69ef0a88e4d
Spiriit33 <37114729+Spiriit33@users.noreply.github.com>
2021-03-25 11:16:12 +0100

    Refactorisation du code
    Amélioration du code en passant les conditions dans des variables.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -6,17 +6,21 @@ class FizzBuzz:
         self.number = number
 
     def play(self):
-        if self.number < 0:
+        multiple_of_three = self.number % 3 == 0
+        multiple_of_five = self.number % 5 == 0
+
+        if self.number <= 0:
             raise InvalidNumberException('Number is invalid.')
-        if self.number == 0:
-            raise InvalidNumberException("Number is invalid")
-        if self.number % 3 == 0 and self.number % 5 == 0:
+        if multiple_of_three and multiple_of_five:
             return "FizzBuzz"
-        if self.number % 3 == 0:
+        if multiple_of_three:
             return "Fizz"
-        if self.number % 5 == 0:
+        if multiple_of_five:
             return 'Buzz'
         else:
             return self.number
+
+
+
 
 
